2f038f5163c393e8b2cca219d9620871fc4fcf69
gitdexgit <ttdextester@gmail.com>
2026-05-13 06:49:44 +0100

    fix(trouble): resolve warnings and lsp noise

--- lua/dex/lazy/trouble.lua
+++ lua/dex/lazy/trouble.lua
@@ -1,20 +1,22 @@
 return {
-    {
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup({})
+	{
+		"folke/trouble.nvim",
+		config = function()
+			require("trouble").setup({})
 
-            vim.keymap.set("n", "<leader>tt", function()
-                require("trouble").toggle("diagnostics")
-            end)
+            -- TODO:
+            --
+			vim.keymap.set("n", "<leader>tt", function()
+				require("trouble").toggle("diagnostics")
+			end)
 
-            vim.keymap.set("n", "[t", function()
-                require("trouble").prev({ skip_groups = true, jump = true })
-            end)
+			vim.keymap.set("n", "[t", function()
+				vim.cmd("Trouble diagnostics prev skip_groups=true jump=true")
+			end)
 
-            vim.keymap.set("n", "]t", function()
-                require("trouble").next({ skip_groups = true, jump = true })
-            end)
-        end
-    },
+			vim.keymap.set("n", "]t", function()
+				vim.cmd("Trouble diagnostics next skip_groups=true jump=true")
+			end)
+		end,
+	},
 }
