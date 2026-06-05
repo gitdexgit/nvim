21ada98d71692af808d68e66b60a0e23355e7a7e
gitdexgit <ttdextester@gmail.com>
2026-06-05 22:19:39 +0100

    refactor(harpoon): changed +[{( to be 1234 numbers because now I use qwerty 100%. and also added F1234 for the smart quick dirty replacing(I rarely use but we will see when building projects)

--- lua/dex/lazy/harpoon.lua
+++ lua/dex/lazy/harpoon.lua
@@ -13,89 +13,115 @@ return {
 				},
 			})
 
+			local function ui_swap(target_idx)
+				if vim.bo.filetype ~= "harpoon" then
+					return
+				end
+
+				-- m' : set mark at current line
+				-- Vy : visual-line yank current line
+				-- %dG: go to target line (1, 2, etc)
+				-- Vp : visual-line paste (replaces target, puts target in register)
+				-- '' : jump back to mark
+				-- Vp : visual-line paste (replaces original with target)
+				local keys = string.format("normal! m'Vy%dGVp''Vp", target_idx)
+				vim.cmd(keys)
+
+				-- Move cursor to the new position of your file
+				vim.cmd("normal! " .. target_idx .. "G")
+			end
+
 			vim.keymap.set("n", "<leader>A", function()
 				harpoon:list():prepend()
 			end, { desc = "Harpoon: Prepend (Set to Slot 1)" })
 
-            -- append to the lest
 			vim.keymap.set("n", "<leader>a", function()
 				harpoon:list():add()
 			end, { desc = "Harpoon: Add File to List" })
 
-			vim.keymap.set("n", "<C-e>", function()
-				harpoon.ui:toggle_quick_menu(harpoon:list(), { height_in_lines = 10 })
-			end, { desc = "Harpoon: Toggle Quick Menu" })
+			-- I just never use it I prefer <leader>h
+			-- vim.keymap.set("n", "<C-e>", function()
+			-- 	harpoon.ui:toggle_quick_menu(harpoon:list(), { height_in_lines = 10 })
+			-- end, { desc = "Harpoon: Toggle Quick Menu" })
 
 			vim.keymap.set("n", "<leader>h", function()
 				harpoon.ui:toggle_quick_menu(harpoon:list(), { height_in_lines = 10 })
 			end, { desc = "Harpoon: Toggle Quick Menu" })
 
-
-            -- programming symbols do the switch.
-			vim.keymap.set("n", "<leader>+", function()
-				harpoon:list():select(1)
-			end, { desc = "Harpoon: Go to 1" })
-			vim.keymap.set("n", "<leader>[", function()
-				harpoon:list():select(2)
-			end, { desc = "Harpoon: Go to 2" })
-			vim.keymap.set("n", "<leader>{", function()
-				harpoon:list():select(3)
-			end, { desc = "Harpoon: Go to 3" })
-			vim.keymap.set("n", "<leader>(", function()
-				harpoon:list():select(4)
-			end, { desc = "Harpoon: Go to 4" })
-			vim.keymap.set("n", "<leader>&", function()
-				harpoon:list():select(5)
-			end, { desc = "Harpoon: Go to 5" })
-			vim.keymap.set("n", "<leader>=", function()
-				harpoon:list():select(6)
-			end, { desc = "Harpoon: Go tho 6" })
-			vim.keymap.set("n", "<leader>)", function()
-				harpoon:list():select(7)
-			end, { desc = "Harpoon: Go tho 7" })
-			vim.keymap.set("n", "<leader>}", function()
-				harpoon:list():select(8)
-			end, { desc = "Harpoon: Go tho 8" })
-			vim.keymap.set("n", "<leader>]", function()
-				harpoon:list():select(9)
-			end, { desc = "Harpoon: Go tho 9" })
-			vim.keymap.set("n", "<leader>*", function()
-				harpoon:list():select(10)
-			end, { desc = "Harpoon: Go tho 10" })
-
-
-
-            -- leader+shift+programming symbols do the swap
+			-- [Select Keymaps]
 			vim.keymap.set("n", "<leader>1", function()
-				harpoon:list():replace_at(1)
-			end, { desc = "Harpoon: Replace Slot 1" })
+				harpoon:list():select(1)
+			end)
 			vim.keymap.set("n", "<leader>2", function()
-				harpoon:list():replace_at(2)
-			end, { desc = "Harpoon: Replace Slot 2" })
+				harpoon:list():select(2)
+			end)
 			vim.keymap.set("n", "<leader>3", function()
-				harpoon:list():replace_at(3)
-			end, { desc = "Harpoon: Replace Slot 3" })
+				harpoon:list():select(3)
+			end)
 			vim.keymap.set("n", "<leader>4", function()
-				harpoon:list():replace_at(4)
-			end, { desc = "Harpoon: Replace Slot 4" })
+				harpoon:list():select(4)
+			end)
 			vim.keymap.set("n", "<leader>5", function()
-				harpoon:list():replace_at(5)
-			end, { desc = "Harpoon: Replace Slot 5" })
+				harpoon:list():select(5)
+			end)
 			vim.keymap.set("n", "<leader>6", function()
-				harpoon:list():replace_at(6)
-			end, { desc = "Harpoon: Replace Slot 6" })
+				harpoon:list():select(6)
+			end)
 			vim.keymap.set("n", "<leader>7", function()
-				harpoon:list():replace_at(7)
-			end, { desc = "Harpoon: Replace Slot 7" })
+				harpoon:list():select(7)
+			end)
 			vim.keymap.set("n", "<leader>8", function()
-				harpoon:list():replace_at(8)
-			end, { desc = "Harpoon: Replace Slot 8" })
+				harpoon:list():select(8)
+			end)
 			vim.keymap.set("n", "<leader>9", function()
+				harpoon:list():select(9)
+			end)
+
+            -- I can't have more than 10 because there is a wait then I type <leader>1
+			-- vim.keymap.set("n", "<leader>10", function()
+			-- 	harpoon:list():select(10)
+			-- end)
+
+			-- [Replace Keymaps]
+			vim.keymap.set("n", "<leader>+", function()
+				harpoon:list():replace_at(1)
+			end)
+			vim.keymap.set("n", "<leader>[", function()
+				harpoon:list():replace_at(2)
+			end)
+			vim.keymap.set("n", "<leader>{", function()
+				harpoon:list():replace_at(3)
+			end)
+			vim.keymap.set("n", "<leader>(", function()
+				harpoon:list():replace_at(4)
+			end)
+			vim.keymap.set("n", "<leader>&", function()
+				harpoon:list():replace_at(5)
+			end)
+			vim.keymap.set("n", "<leader>=", function()
+				harpoon:list():replace_at(6)
+			end)
+			vim.keymap.set("n", "<leader>)", function()
+				harpoon:list():replace_at(7)
+			end)
+			vim.keymap.set("n", "<leader>}", function()
+				harpoon:list():replace_at(8)
+			end)
+			vim.keymap.set("n", "<leader>]", function()
 				harpoon:list():replace_at(9)
-			end, { desc = "Harpoon: Replace Slot 9" })
-			vim.keymap.set("n", "<leader>10", function()
-				harpoon:list():replace_at(10)
-			end, { desc = "Harpoon: Replace Slot 10" })
+			end)
+
+            -- if there are no <leader>10 then what is the point of <leader>*
+			-- vim.keymap.set("n", "<leader>*", function()
+			-- 	harpoon:list():replace_at(10)
+			-- end)
+
+			-- NEW: Leader + Physical F-keys (F1, F2, etc.)
+			for i = 1, 10 do
+				vim.keymap.set("n", "<leader><F" .. i .. ">", function()
+					ui_swap(i)
+				end, { desc = "Harpoon UI: Swap with Slot " .. i })
+			end
 		end,
 	},
 }
