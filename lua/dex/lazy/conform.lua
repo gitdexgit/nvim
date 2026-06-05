f256a255cf99761be03b957a52d2232a6134f890
gitdexgit <ttdextester@gmail.com>
2026-06-05 22:15:29 +0100

    feature(conform): added python to conform

--- lua/dex/lazy/conform.lua
+++ lua/dex/lazy/conform.lua
@@ -5,11 +5,12 @@ return {
 		require("conform").setup({
 			format_on_save = {
 				timeout_ms = 5000,
-                lsp_format = "fallback",
+				lsp_format = "fallback",
 			},
 			formatters_by_ft = {
 				c = { "clang-format" },
 				cpp = { "clang-format" },
+				python = { "black" },
 				lua = { "stylua" },
 				go = { "gofmt" },
 				javascript = { "prettier" },
