86858ede5edafe316d1b4e6e208edb0c13526c3b
gitdexgit <ttdextester@gmail.com>
2026-06-09 21:25:08 +0100

    change(vim-visual-multi): keybinds - multiple cursors keybinds changed to try to match emacs ones

--- lua/dex/lazy/vim-visual-multi.lua
+++ lua/dex/lazy/vim-visual-multi.lua
@@ -2,12 +2,21 @@ return {
     'mg979/vim-visual-multi',
     branch = 'master',
     init = function()
-        -- Define your custom mappings here
         vim.g.VM_maps = {
-            ['Add Cursor Down']             = '<A-J>',
-            ['Add Cursor Up']               = '<A-K>',
-            ['Select Cursor Down']          = '<C-A-j>', -- Optional: Alt+Shift+J to select lines down
-            ['Select Cursor Up']            = '<C-A-k>', -- Optional: Alt+Shift+K to select lines up
+            ['Find Under']    = '<leader>>',
+            ['Find Prev']     = '<leader><',
+            ['Skip Region']   = '<C-|>',     -- Skip next
+            ['Remove Region'] = '<C-S-q>',   -- VM lacks "skip back", use remove
         }
+    end,
+    config = function()
+        -- Normal mode
+        vim.keymap.set('n', '<C-S-c><C-S-c>', '<Plug>(VM-Add-Cursor-At-Pos)')
+
+        -- Insert mode
+        vim.keymap.set('i', '<C-x>>', '<Plug>(VM-Find-Under)')
+        vim.keymap.set('i', '<C-x><', '<Plug>(VM-Find-Prev)')
+        -- Skip in Insert
+        vim.keymap.set('i', '<C-x>|', '<Plug>(VM-Skip-Region)')
     end
 }
