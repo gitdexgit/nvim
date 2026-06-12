05efdb41ce61c25a5529c331bed4074667f16b47
gitdexgit <ttdextester@gmail.com>
2026-06-12 23:37:51 +0100

    change(yanky): Change keybind M-p for A-p, a trade had to be made.

--- lua/dex/lazy/yanky.lua
+++ lua/dex/lazy/yanky.lua
@@ -10,10 +10,10 @@ return {
 		{ "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" } },
 		{ "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" } },
 		{ "<M-y>", "<Plug>(YankyCycleForward)", mode = "n" },
-		{ "<M-p>", "<Plug>(YankyCycleBackward)", mode = "n" },
+		{ "<M-S-p>", "<Plug>(YankyCycleBackward)", mode = "n" },
 		-- Insert mode mappings
 		{ "<C-y>", "<C-o><Plug>(YankyPutAfter)", mode = "i", desc = "Put from Yanky" },
 		{ "<M-y>", "<C-o><Plug>(YankyCycleForward)", mode = "i", desc = "Cycle Yanky Forward" },
-		{ "<M-p>", "<C-o><Plug>(YankyCycleBackward)", mode = "i", desc = "Cycle Yanky Backward" },
+		{ "<M-S-p>", "<C-o><Plug>(YankyCycleBackward)", mode = "i", desc = "Cycle Yanky Backward" },
 	},
 }
