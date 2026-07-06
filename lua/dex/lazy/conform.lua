1930309a039e53747f23aaad67d8db8751187d33
gitdexgit <ttdextester@gmail.com>
2026-07-06 17:32:42 +0100

    make conform less annoying it was converting spaces to tabs for some reason now it's fixed

--- lua/dex/lazy/conform.lua
+++ lua/dex/lazy/conform.lua
@@ -1,31 +1,37 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			format_on_save = {
-				timeout_ms = 5000,
-				lsp_format = "fallback",
-			},
-			formatters_by_ft = {
-				c = { "clang-format" },
-				cpp = { "clang-format" },
-				python = { "black" },
-				lua = { "stylua" },
-				go = { "gofmt" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				elixir = { "mix" },
-			},
-			formatters = {
-				["clang-format"] = {
-					prepend_args = { "-style=file", "-fallback-style=LLVM" },
-				},
-			},
-		})
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        require("conform").setup({
+            format_on_save = {
+                timeout_ms = 5000,
+                lsp_format = "fallback",
+            },
+            formatters_by_ft = {
+                c = { "clang-format" },
+                cpp = { "clang-format" },
+                python = { "black" },
+                lua = { "stylua" },
+                go = { "gofmt" }, -- Note: Go forces tabs by design
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                elixir = { "mix" },
+            },
+            formatters = {
+                ["clang-format"] = {
+                    prepend_args = { "--style={IndentWidth: 4, UseTab: Never}" },
+                },
+                ["stylua"] = {
+                    prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+                },
+                ["prettier"] = {
+                    prepend_args = { "--tab-width", "4", "--use-tabs", "false" },
+                },
+            },
+        })
 
-		vim.keymap.set("n", "<leader>f", function()
-			require("conform").format({ bufnr = 0 })
-		end)
-	end,
+        vim.keymap.set("n", "<leader>f", function()
+            require("conform").format({ bufnr = 0 })
+        end)
+    end,
 }
