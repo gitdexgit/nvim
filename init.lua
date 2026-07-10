00204b0452151cbb9e40228174d31c611a2bbd9c
gitdexgit <ttdextester@gmail.com>
2026-07-10 19:42:28 +0100

    idk what changed in this no clue

--- init.lua
+++ init.lua
@@ -38,44 +38,44 @@ vim.o.titlestring = "%f %m - %l,%c"
 
 -- Force title update on cursor move (so line/col updates instantly)
 vim.api.nvim_create_autocmd("CursorMoved", {
-	pattern = "*",
-	callback = function()
-		vim.o.titlestring = "%f %m - %l,%c"
-	end,
+    pattern = "*",
+    callback = function()
+        vim.o.titlestring = "%f %m - %l,%c"
+    end,
 })
 
 -- Also update title when buffer changes (e.g., :w)
 vim.api.nvim_create_autocmd("BufWritePost", {
-	pattern = "*",
-	callback = function()
-		vim.o.titlestring = "%f %m - %l,%c"
-	end,
+    pattern = "*",
+    callback = function()
+        vim.o.titlestring = "%f %m - %l,%c"
+    end,
 })
 
 vim.o.title = true
 local function update_title()
-	local fname = vim.fn.expand("%:t")
-	if fname == "" then
-		fname = "[No Name]"
-	end
-	local mod = vim.bo.modified and " +" or ""
-	local line = vim.fn.line(".")
-	local col = vim.fn.col(".")
-	vim.o.titlestring = string.format("%s%s - %d,%d", fname, mod, line, col)
+    local fname = vim.fn.expand("%:t")
+    if fname == "" then
+        fname = "[No Name]"
+    end
+    local mod = vim.bo.modified and " +" or ""
+    local line = vim.fn.line(".")
+    local col = vim.fn.col(".")
+    vim.o.titlestring = string.format("%s%s - %d,%d", fname, mod, line, col)
 end
 vim.api.nvim_create_autocmd({ "CursorMoved", "BufWritePost", "BufEnter" }, { callback = update_title })
 update_title()
 
 local bars_on = true
 function _G.toggle_bars()
-	if bars_on then
-		vim.opt.laststatus = 0
-		bars_on = false
-	else
-		-- Only show the bottom bar
-		vim.opt.laststatus = 3
-		bars_on = true
-	end
+    if bars_on then
+        vim.opt.laststatus = 0
+        bars_on = false
+    else
+        -- Only show the bottom bar
+        vim.opt.laststatus = 3
+        bars_on = true
+    end
 end
 
 vim.keymap.set("n", "<leader>z", _G.toggle_bars)
